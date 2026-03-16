5bf663b84409faa79c7eb2f6377118daae7f438f
luciaSoto <lucias.emerxente1@gmail.com>
2026-03-16 09:04:45 +0100

    Publicacion Facebook

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { DesktopFacebookPost } from './components/desktop-facebook-post/desktop-facebook-post';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'desktop-post',
+    component: DesktopFacebookPost,
+    title: 'Facebook Desktop Post'
+  },
+  {
+    path: '**',
+    redirectTo: 'desktop-post'
+  }
+];
---
+++ src/app/components/desktop-facebook-post/desktop-facebook-post.ts
@@ -0,0 +1,29 @@
+import { JsonPipe } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-desktop-facebook-post',
+  imports: [JsonPipe],
+  templateUrl: './desktop-facebook-post.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DesktopFacebookPost {
+
+  data: any = {};
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<any>('/data.json').subscribe({
+      next: (respuesta) => {
+        console.log(respuesta);
+        this.data = respuesta.post;
+      },
+      error: (err) => {
+        console.error('Error cargando el JSON:', err);
+      }
+    });
+  }
+}
+
